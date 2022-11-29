bb6f1127a29407b4034504e8f6f6ca9484cf1ea1
Carmelore <47265512+Carmelore@users.noreply.github.com>
2022-11-29 10:05:55 -0500

    Improved initial SQL for DB setup
    + Cleaned up Products queries
    + Added Users table

--- initial-queries.sql
+++ initial-queries.sql
@@ -1,4 +1,7 @@
-CREATE TABLE Product (
+--DROP TABLE Products;
+--DROP TABLE Users;
+
+CREATE TABLE Products (
 id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
 name VARCHAR(60) NOT NULL UNIQUE,
 description VARCHAR(240),
@@ -6,14 +9,20 @@ brand VARCHAR(60) NOT NULL,
 type VARCHAR(60) NOT NULL,
 price DECIMAL(5, 2) NOT NULL,
 quantity INT NOT NULL,
-CONSTRAINT primary_key PRIMARY KEY (id)
-)
+image BLOB,
+PRIMARY KEY (id)
+);
 
-ALTER TABLE product ADD image BLOB;
+INSERT INTO products(name, description, brand, type, price, quantity) VALUES ('product 1', 'the first product', 'generic', 'product', 9.99, 20);
+INSERT INTO products(name, description, brand, type, price, quantity) VALUES ('product 2', 'the second product', 'generic', 'product', 9.99, 20);
+INSERT INTO products(name, description, brand, type, price, quantity) VALUES ('product 3', 'the third product', 'special', 'product', 99.99, 2);
+INSERT INTO products(name, description, brand, type, price, quantity) VALUES ('product 4', 'the fourth product', 'generic', 'non-product', 9.99, 200);
 
-INSERT INTO product(name, description, brand, type, price, quantity) VALUES ('product 1', 'the first product', 'generic', 'product', 9.99, 20);
-INSERT INTO product(name, description, brand, type, price, quantity) VALUES ('product 2', 'the second product', 'generic', 'product', 9.99, 20);
-INSERT INTO product(name, description, brand, type, price, quantity) VALUES ('product 3', 'the third product', 'special', 'product', 99.99, 2);
-INSERT INTO product(name, description, brand, type, price, quantity) VALUES ('product 4', 'the fourth product', 'generic', 'non-product', 9.99, 200);
-
-SELECT * FROM product;
+CREATE TABLE Users (
+id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+name VARCHAR(60) NOT NULL,
+username VARCHAR(20) NOT NULL UNIQUE,
+password LONG VARCHAR NOT NULL,
+admin BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id)
+);
