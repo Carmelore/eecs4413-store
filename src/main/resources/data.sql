2610faabc0b94347d1920bdb456273709a3fd511
Carmela Leung <carmela.mn.leung@gmail.com>
2022-12-09 11:48:00 -0500

    Seeds

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,6 +9,19 @@ INSERT INTO users(name, username, password, admin) VALUES ('Adam', 'Addma', 'pas
 INSERT INTO users(name, username, password, admin) VALUES ('Daniel', 'Mandokero', 'password', true);
 INSERT INTO users(name, username, password, admin) VALUES ('Non-Admin User', 'username', 'password', false);
 
---INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES ();
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (1, 1, 1, '2022-12-09');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (1, 3, 2, '2022-12-09');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (2, 1, 10, '2022-12-09');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (2, 4, 1, '2022-12-03');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (2, 3, 20, '2022-11-09');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (3, 1, 1, '2022-12-04');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (3, 1, 1, '2022-12-05');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (3, 1, 1, '2022-12-09');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (4, 2, 11, '2021-12-09');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (4, 2, 11, '2022-11-29');
+INSERT INTO sales(user_id, product_id, quantity, created_at) VALUES (4, 2, 11, '2022-12-09');
 
---INSERT INTO visits(ip_address) VALUES ();
+INSERT INTO visits(ip_address, created_at, product_id, status) VALUES ('159.203.8.58', '2022-12-09', 1, 'VIEWED');
+INSERT INTO visits(ip_address, created_at, product_id, status) VALUES ('54.225.144.155', '2022-12-07', 3, 'IN_CART');
+INSERT INTO visits(ip_address, created_at, product_id, status) VALUES ('54.8.33.173', '2022-12-05', 4, 'PURCHASED');
+INSERT INTO visits(ip_address, created_at, product_id, status) VALUES ('54.8.33.173', '2022-12-04', 4, 'VIEWED');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -35,6 +35,6 @@ id SERIAL,
 ip_address VARCHAR(20) NOT NULL,
 created_at DATE NOT NULL,
 product_id INT NOT NULL,
-status VARCHAR(12) NOT NULL DEFAULT 'VIEWED', -- Proposed options: VIEWED, IN_CART, PURCHASED
+status VARCHAR(12) NOT NULL DEFAULT 'VIEWED',
 FOREIGN KEY (product_id) REFERENCES Products
 );
