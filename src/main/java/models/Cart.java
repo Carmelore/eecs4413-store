64f047dcebfdbf7f490316c39f381392691cad14
Daniel R <danielrios3000@gmail.com>
2022-12-15 01:56:29 -0500

    Methods to calculate total price, subtotal, apply discounts, etc

--- src/main/java/models/Cart.java
+++ src/main/java/models/Cart.java
@@ -3,14 +3,20 @@ package models;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.context.annotation.Scope;
+
+import models.Discount.discount_type;
 
 @Scope
 public class Cart {
 	private Map<Product, Integer> items;
+	private Map<Integer, Discount> discounts;
 	private double appliedDiscounts;
 	private double percentageDiscount;
+	private double subtotal;
+	private double totalPrice;
 
 	public Cart() {
 		this.items = new HashMap<>();
@@ -26,6 +32,22 @@ public class Cart {
 		}
 
 		items.put(product, quantity);
+
+		updateSubtotal();
+	}
+
+	public void addDiscount(Discount discount) {
+		if (discount == null || discount.getUsed()) {
+			throw new IllegalArgumentException("Invalid discount or code has been used already");
+		}
+
+		if (items.containsKey(discount.getId())) {
+			throw new IllegalArgumentException("Discount already applied");
+		}
+
+		discounts.put(discount.getId(), discount);
+
+		updateDiscounts();
 	}
 
 	public void removeItem(Product product) {
@@ -52,18 +74,29 @@ public class Cart {
 		return items.values().stream().mapToInt(Integer::intValue).sum();
 	}
 
-	public double getTotalPrice() {
-		double totalPrice = items.entrySet().stream().mapToDouble(entry -> entry.getKey().getPrice() * entry.getValue())
-				.sum();
-		if (appliedDiscounts > 0) {
-			totalPrice -= appliedDiscounts;
-		}
-		if (percentageDiscount > 0) {
-			if (percentageDiscount > 100) {
-				percentageDiscount = 100;
+	private void updateSubtotal() {
+		subtotal = items.entrySet().stream().mapToDouble(entry -> entry.getKey().getPrice() * entry.getValue()).sum();
+	}
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	private void updateDiscounts() {
+		for (Entry<Integer, Discount> discount : discounts.entrySet()) {
+			if (discount.getValue().getType() == discount_type.total
+					&& subtotal > discount.getValue().getRequired_spending()) {
+				appliedDiscounts += discount.getValue().getDiscount();
+			} else {
+				percentageDiscount += discount.getValue().getDiscount();
 			}
-			totalPrice = totalPrice - (totalPrice * (percentageDiscount / 100));
+			discount.getValue().setUsed(true);
 		}
+	}
+
+	public double getTotalPrice() {
+		subtotal -= appliedDiscounts;
+		subtotal = subtotal - (subtotal * (percentageDiscount / 100));
 		return totalPrice;
 	}
 }
