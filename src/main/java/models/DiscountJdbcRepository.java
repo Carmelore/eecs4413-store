8a91333ccd1df5c5f836dbe4accb6198ade78838
Daniel R <danielrios3000@gmail.com>
2022-12-16 18:25:14 -0500

    Discount Controller add

--- src/main/java/models/DiscountJdbcRepository.java
+++ src/main/java/models/DiscountJdbcRepository.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import models.Discount.discount_type;
+
+@Repository
 public class DiscountJdbcRepository {
+	
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 
@@ -16,7 +22,7 @@ public class DiscountJdbcRepository {
 	}
 
 	public Discount findByDiscountCode(String code) {
-		return jdbcTemplate.queryForObject("select * from Discounts where Discountname=?",
+		return jdbcTemplate.queryForObject("select * from Discounts where code=?",
 				new BeanPropertyRowMapper<>(Discount.class), code);
 	}
 
@@ -26,7 +32,7 @@ public class DiscountJdbcRepository {
 		return results;
 	}
 
-	public int create(String code, String discount, String requiredSpending, String type) {
+	public int create(String code, int discount, int requiredSpending, discount_type type) {
 		if (validateNewDiscount(code, discount, requiredSpending, type)) {
 			return jdbcTemplate.update("insert into Discounts(code, discount, requiredSpending, type) values(?,?,?,?,?)",
 					code, discount, requiredSpending, type, false);
@@ -35,11 +41,11 @@ public class DiscountJdbcRepository {
 		}
 	}
 
-	public boolean validateNewDiscount(String code, String discount, String requiredSpending, String type) {
+	public boolean validateNewDiscount(String code, int discount, int requiredSpending, discount_type type) {
 		if ((code.length() > 15 || code.length() < 0)
-				|| Integer.parseInt(discount) < 0
-				|| !(type.equals("total") || type.equals("percent"))
-				|| (type.equals("percent") && (Integer.parseInt(discount) > 100))) {
+				|| discount < 0
+				|| !(type.equals(discount_type.total) || type.equals(discount_type.percent))
+				|| (type.equals(discount_type.percent) && discount > 100)) {
 			return false;
 		} else {
 			return true;
