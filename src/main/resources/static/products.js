e72e402acf0c569412106ad4f0161f33ba72b6bd
Daniel R <danielrios3000@gmail.com>
2022-12-17 01:04:31 -0500

    discounts applied when adding to cart

--- src/main/resources/static/products.js
+++ src/main/resources/static/products.js
@@ -3,32 +3,56 @@ angular.module('Group-V_Store').controller('ProductsController', function($scope
 	const urlParams = new URLSearchParams(queryString);
 	const type = urlParams.get('type') ?? '';
 	const brand = urlParams.get('brand') ?? '';
-	
+
 	$http.get(`http://localhost:8080/products?type=${type}&brand=${brand}`)
 		.then(function(response) {
 			$scope.products = response.data;
 		});
-	
+
 	$scope.logVisit = function(productId, status) {
 		$http.get('https://api.ipify.org/?format=json')
 			.then((response) => {
-				$http.post('/visits', {ip_address: response.data.ip, product: productId, status: status});
+				$http.post('/visits', { ip_address: response.data.ip, product: productId, status: status });
 			});
 	}
 	$scope.addToCart = function(product) {
 		//$window.sessionStorage.removeItem("cart");
-	let cart = JSON.parse(sessionStorage.getItem("cart"));
-	console.log(cart);
-	let index = cart.items.findIndex(item => item.name === product.name && item.id === product.id);
-	if (index != -1){
-		cart.items[index].amount += 1;
-	} else {
-		product.amount = 1;
-		cart.items.push(product);
-	}
-	cart.totalQuantity += 1;
-	cart.totalPrice += product.price;
-	sessionStorage.setItem("cart", JSON.stringify(cart));
+		let cart = JSON.parse(sessionStorage.getItem("cart"));
+		console.log(cart);
+		let index = cart.items.findIndex(item => item.name === product.name && item.id === product.id);
+		if (index != -1) {
+			cart.items[index].amount += 1;
+		} else {
+			product.amount = 1;
+			cart.items.push(product);
+		}
+		cart.totalQuantity = 0;
+		var subtotal = 0;
+		var totalDiscounts = 0;
+		var percentageDiscount = 0;
+		console.log(cart.discounts[0].type);
+		for (let i = 0; i < cart.items.length; i++) {
+			subtotal += cart.items[i].price * cart.items[i].amount;
+			cart.totalQuantity += cart.items[i].amount;
+		}
+		cart.totalPrice = subtotal;
+		for (let i = 0; i < cart.discounts.length; i++) {
+			// 
+			console.log(cart.discounts[i].type === "total")
+			if (cart.discounts[i].type === "total" && cart.discounts[i].required_spending < subtotal) {
+				totalDiscounts += cart.discounts[i].discount;
+			}
+			//
+			if (cart.discounts[i].type === "percent" && cart.discounts[i].required_spending < subtotal) {
+				percentageDiscount += cart.discounts[i].discount;
+			}
+		}
+		cart.totalPrice -= totalDiscounts;
+		cart.totalPrice = cart.totalPrice * (1 - (percentageDiscount / 100));
+		if (cart.totalPrice < 0) {
+			cart.totalPrice = 0;
+		}
+		sessionStorage.setItem("cart", JSON.stringify(cart));
 	}
 });
 
